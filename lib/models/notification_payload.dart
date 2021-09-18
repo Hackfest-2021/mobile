1bafdf81532fd45ee98cdff778b6ce106a073d84
Saad Ismail <saad.ismail@careem.com>
2021-09-19 00:22:57 +0500

    remove type from NotificationPayload, and parse string for alertId

--- lib/models/notification_payload.dart
+++ lib/models/notification_payload.dart
@@ -2,27 +2,33 @@ import 'dart:convert';
 
 class NotificationPayload {
 
-  final String type;
   final int alertId;
   final String alertName;
   final String driverName;
 
-  NotificationPayload({required this.type, required this.alertId, required this.alertName, required this.driverName});
+  NotificationPayload({required this.alertId, required this.alertName, required this.driverName});
 
   factory NotificationPayload.fromJsonString(String str) => NotificationPayload.fromJson(jsonDecode(str));
 
   String toJsonString() => jsonEncode(_toJson());
 
-  factory NotificationPayload.fromJson(Map<String, dynamic> json) => NotificationPayload(
-    type: json['type'] ?? "",
-    alertId: json['alertId'] ?? 0,
-    alertName: json['alertName'] ?? "",
-    driverName: json['driverName'] ?? "",
-  );
+  factory NotificationPayload.fromJson(Map<String, dynamic> json) {
+    int alertId;
+    if (json['alertId'] is int) {
+      alertId = json['alertId'];
+    } else {
+      alertId = int.parse(json['alertId'] ?? "0");
+    }
+
+    return NotificationPayload(
+      alertId: alertId,
+      alertName: json['alertName'] ?? "",
+      driverName: json['driverName'] ?? "",
+    );
+  }
 
 
   Map<String, dynamic> _toJson() => {
-    'type': type,
     'alertId': alertId,
     'alertName': alertName,
     'driverName': driverName,
