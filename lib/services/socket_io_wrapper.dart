5f939c605f3908c758fbfbe84c26017cfa8ebc43
Saad Ismail <saad.ismail@careem.com>
2021-09-19 16:11:50 +0500

    uncomment code for sending video stream

--- lib/services/socket_io_wrapper.dart
+++ lib/services/socket_io_wrapper.dart
@@ -1,35 +1,29 @@
-// import 'dart:async';
-// import 'dart:convert';
-//
-// import 'package:flutter/cupertino.dart';
-// import 'package:web_socket_channel/web_socket_channel.dart';
-// import 'package:web_socket_channel/status.dart' as status;
-// import 'package:websocket_channel_wrapper/websocket_channel_wrapper.dart';
-// // import 'package:websocket/websocket.dart';
+import 'dart:convert';
+
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 class SocketIOWrapper {
   var channel;
   SocketIOWrapper() {
-    //  channel = WebSocketChannel.connect(
-    //   Uri.parse('ws://192.168.100.53:8000/ws/'),
-    // );
-    //
-    // channel.sink.add(json.encode({
-    //   "action": "subscribe_to_DeviceSettings",
-    //   "request_id": "sssdfk",
-    // }));
+     channel = WebSocketChannel.connect(
+      Uri.parse('ws://192.168.78.186:8000/ws/'),
+    );
+
+    channel.sink.add(json.encode({
+      "action": "subscribe_to_DeviceSettings",
+      "request_id": "sssdfk",
+    }));
   }
 
   sendData(bytes) {
-    // String base64Image = base64Encode(bytes);
-    // print("sending data");
-    // // print(base64Image);
-    // this.channel.sink.add(
-    //   json.encode({
-    //     "action": "subscribe_to_DeviceSettings",
-    //     "request_id": "sssdfk",
-    //     "data": "${base64Image}"
-    //   })
-    // );
+    String base64Image = base64Encode(bytes);
+    print("sending data");
+    this.channel.sink.add(
+      json.encode({
+        "action": "subscribe_to_DeviceSettings",
+        "request_id": "sssdfk",
+        "data": "${base64Image}"
+      })
+    );
   }
 }
